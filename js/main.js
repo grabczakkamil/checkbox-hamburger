9f0404421b3f8e5f614bd4faa60ae8cde230ceb5
Kamil Grabczak <grabczakkamil@gmail.com>
2018-07-19 20:46:02 +0200

    burger w html i css + checkboxy

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$('input[type="checkbox"]').change(function(e) {
+    'use strict';
+
+  var checked = $(this).prop("checked"),
+      container = $(this).parent(),
+      siblings = container.siblings();
+
+  container.find('input[type="checkbox"]').prop({
+    indeterminate: false,
+    checked: checked
+  });
+
+  function checkSiblings(el) {
+
+    var parent = el.parent().parent(),
+        all = true;
+
+    el.siblings().each(function() {
+      return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+    });
+
+    if (all && checked) {
+
+      parent.children('input[type="checkbox"]').prop({
+        indeterminate: false,
+        checked: checked
+      });
+
+      checkSiblings(parent);
+
+    } else if (all && !checked) {
+
+      parent.children('input[type="checkbox"]').prop("checked", checked);
+      parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+      checkSiblings(parent);
+
+    } else {
+
+      el.parents("li").children('input[type="checkbox"]').prop({
+        indeterminate: true,
+        checked: false
+      });
+
+    }
+
+  }
+
+  checkSiblings(container);
+});
+
+ $(document).ready(function(){
+        $('#top-roll').click(function(){
+            if($(this).is(":checked")){
+                $(".bun-1").addClass("bun-1-show")  
+            }
+            else {
+                 $(".bun-1").removeClass("bun-1-show");   
+                };
+        });
+     });
+
